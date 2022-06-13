405d7b8b6c8399ba183d80674c52e28f998472f4
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-06-13 18:55:12 -0500

    Se agregó el Splash funcionando, también el MainViewModel funcionando, se solucionaron ciertos errores

--- app/src/main/java/com/example/cartalibre/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/cartalibre/ui/main/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.example.cartalibre.ui.main
 
-import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.example.cartalibre.databinding.ActivityMainBinding
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
-    private var total: Int = 0
-    private var total_martini: Int = 0
-    private var total_cuba: Int = 0
-    private var total_mojito: Int = 0
-    private var total_gintonic: Int = 0
+    private lateinit var mainViewModel: MainViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,48 +17,66 @@ class MainActivity : Activity() {
         val view = mainBinding.root
         setContentView(view)
 
-        with(mainBinding){
-            pedirButton.setOnClickListener{
-                if(martiniCheckBox.isChecked) {
-                    val cantidad_mar = martiniSpinner.selectedItem.toString().toInt()
-                    total_martini = 25000 * cantidad_mar
-                }
-                else total_martini = 0
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
-                if (cubalibreCheckBox2.isChecked) {
-                    val cantidad_cuba = cubalibreSpinner2.selectedItem.toString().toInt()
-                    total_cuba = 10000 * cantidad_cuba
-                }
-                else total_cuba = 0
-
-                if (mojitoCheckBox3.isChecked) {
-                    val cantidad_mojito = mojitoSpinner3.selectedItem.toString().toInt()
-                    total_mojito = 20000 * cantidad_mojito
-                }
-                else total_mojito = 0
-
-                if (gintonicCheckBox.isChecked) {
-                    val cantidad_gintonic = gintonicSpinner4.selectedItem.toString().toInt()
-                    total_gintonic = 25000 * cantidad_gintonic
-                }
-                else total_gintonic = 0
-
-                var salida = total_martini + total_cuba + total_mojito + total_gintonic
-                resultadoTextView4.text = salida.toString()
-            }
-            cancelButton2.setOnClickListener{
-                total = 0
-                val salida = total.toString()
-                resultadoTextView4.text = salida
-                martiniCheckBox.isChecked = false
-                cubalibreCheckBox2.isChecked = false
-                mojitoCheckBox3.isChecked = false
-                gintonicCheckBox.isChecked =false
-                martiniSpinner.setSelection(0)
-                cubalibreSpinner2.setSelection(0)
-                mojitoSpinner3.setSelection(0)
-                gintonicSpinner4.setSelection(0)
-            }
+        mainViewModel.mutableTotalDone.observe(this) { mutableTotal ->
+            mainBinding.resultadoTextView4.text = mutableTotal.toString()
         }
+
+        fun checkSpinners() {
+            mainBinding.martiniCheckBox.isChecked = mainBinding.martiniSpinner.selectedItem != 0
+            mainBinding.cubalibreCheckBox2.isChecked = mainBinding.cubalibreSpinner2.selectedItem != 0
+            mainBinding.mojitoCheckBox3.isChecked = mainBinding.mojitoSpinner3.selectedItem != 0
+            mainBinding.gintonicCheckBox.isChecked = mainBinding.gintonicSpinner4.selectedItem != 0
+        }
+        //ESTE COMANDO DA ERROR
+        //Prueba de Primer Spinner
+        //mainBinding.martiniSpinner.setOnClickListener {
+        //    checkSpinners()
+        //}
+
+        mainBinding.pedirButton.setOnClickListener {
+            if (mainBinding.martiniCheckBox.isChecked) {
+                val cantidad_mar = mainBinding.martiniSpinner.selectedItem.toString().toInt()
+                mainViewModel.calcularTotalProducto(1, cantidad_mar)
+            }
+            //else total_martini = 0
+
+            if (mainBinding.cubalibreCheckBox2.isChecked) {
+                val cantidad_cuba = mainBinding.cubalibreSpinner2.selectedItem.toString().toInt()
+                mainViewModel.calcularTotalProducto(2, cantidad_cuba)
+
+            }
+            //else total_cuba = 0
+
+            if (mainBinding.mojitoCheckBox3.isChecked) {
+                val cantidad_mojito = mainBinding.mojitoSpinner3.selectedItem.toString().toInt()
+                mainViewModel.calcularTotalProducto(3, cantidad_mojito)
+
+            }
+            //else total_mojito = 0
+
+            if (mainBinding.gintonicCheckBox.isChecked) {
+                val cantidad_gintonic = mainBinding.gintonicSpinner4.selectedItem.toString().toInt()
+                mainViewModel.calcularTotalProducto(4, cantidad_gintonic)
+            }
+            //else total_gintonic = 0
+
+            mainViewModel.calcularTotalCuenta()
+        }
+        mainBinding.cancelButton2.setOnClickListener {
+            mainViewModel.borrarTotalCuenta()
+            mainBinding.martiniCheckBox.isChecked = false
+            mainBinding.cubalibreCheckBox2.isChecked = false
+            mainBinding.mojitoCheckBox3.isChecked = false
+            mainBinding.gintonicCheckBox.isChecked = false
+            mainBinding.martiniSpinner.setSelection(0)
+            mainBinding.cubalibreSpinner2.setSelection(0)
+            mainBinding.mojitoSpinner3.setSelection(0)
+            mainBinding.gintonicSpinner4.setSelection(0)
+        }
+
     }
+
+
 }
--- app/src/main/java/com/example/cartalibre/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/cartalibre/ui/main/MainViewModel.kt
@@ -2,10 +2,41 @@ package com.example.cartalibre.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
-class MainViewModel : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_view_model)
+class MainViewModel : ViewModel() {
+
+    private var totalMartini = 0
+    private var totalCubaLibre = 0
+    private var totalMojito = 0
+    private var totalGintonic = 0
+
+    private var price_martini = 25000
+    private var price_cuba_libre = 10000
+    private var price_mojito = 20000
+    private var price_gintonic = 25000
+
+
+
+    private val mutableTotal: MutableLiveData<Int> = MutableLiveData()
+    val mutableTotalDone: LiveData<Int> = mutableTotal
+
+
+
+    fun calcularTotalProducto(product: Int, quantity: Int){
+        when(product){
+            1 -> totalMartini = price_martini*quantity
+            2 -> totalCubaLibre = price_cuba_libre*quantity
+            3 -> totalMojito = price_mojito*quantity
+            4 -> totalGintonic = price_gintonic*quantity
+        }
+    }
+    fun calcularTotalCuenta(){
+        mutableTotal.value = totalMartini + totalCubaLibre + totalMojito + totalGintonic
+    }
+    fun borrarTotalCuenta(){
+        mutableTotal.value = 0
     }
 }
