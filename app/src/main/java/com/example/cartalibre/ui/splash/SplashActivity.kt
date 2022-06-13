a1adbbe34700bea64f44c23d69d0b244f72c431c
Sebastian Ospina Garcia <sebastian47121@gmail.com>
2022-06-13 16:49:59 -0500

    Se creó el Splash

--- app/src/main/java/com/example/cartalibre/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/cartalibre/ui/splash/SplashActivity.kt
@@ -1,11 +1,32 @@
 package com.example.cartalibre.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cartalibre.databinding.ActivitySplashBinding
+import com.example.cartalibre.ui.main.MainActivity
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashBinding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = splashBinding.root
+        setContentView(view)
+        val timer = Timer()
+        timer.schedule(
+            timerTask {
+                goToMainActivity()
+            }, 1000
+        )
+    }
+    private fun goToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
